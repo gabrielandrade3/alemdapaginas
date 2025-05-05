ca1efa0980886b203878df116255e10c2153643a
gitmilena7 <moreiracarla012@gmail.com>
2025-05-05 15:57:59 -0300

    exfuncao2.php

---
+++ exfuncao2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Função - Valor Padrão</title>
+</head>
+<body>
+
+<?php
+//     function ola($nome = "Zebedeu", $idade = 45){
+// echo "Olá $nome! Você tem $idade anos";
+
+//     }
+//     ola("Ariovaldo", 45);
+//     echo "<br>";
+//     ola();
+//     echo "<br>";
+//     ola("Letsgo da Silva");
+//     echo "<br>";
+//     ola("Berta Rachou", 22);
+
+    // function boasVindas(){
+    //     echo "<br>";
+    // }
+    // boasVindas();
+    // boasVindas();
+    // boasVindas();
+    // boasVindas();
+
+    // function maximo(...$args){
+    //     return max($args);
+
+    // }
+    // function minimo(...$args){
+    //     return min($args);
+    // }
+    // echo "Maior entre 1, 2, 10, -1, 50: ".maximo(1, 2, 10, -1, 50);
+    // echo "<br>";
+    // echo "Menor entre 1, 2, 10, -1, 50:".minimo(1, 2, 10, -1, 50);
+    // echo '<hr>';
+
+    // $numeros = range(1, 100);
+    // // print_r($numeros);
+    // echo maximo(...$numeros); // retornar o maior
+    // echo "<br>";
+    // echo minimo(...$numeros); //retornar o menor
+
+    // function soma(...$args){
+    //     return array_sum($args);
+    // }
+    // echo "<br>";
+    // echo soma(...$numeros);
+?>
+</body>
+</html>
